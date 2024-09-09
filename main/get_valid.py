0ac700ae60e74a4ba1930af7d7504a229da84367
Tomislav Simovic <116288908+shz0@users.noreply.github.com>
2024-09-09 11:29:05 +0200

    Update get_valid.py

--- main/get_valid.py
+++ main/get_valid.py
@@ -1,1 +1,6 @@
-
+def get_valid_input(prompt: str, validate_func, error_msg: str) -> str:
+    while True:
+        user_input = input(prompt)
+        if validate_func(user_input):
+            return user_input
+        print(error_msg)
