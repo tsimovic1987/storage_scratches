1f6a08e71caf580fc883870eeaf4ce7f05136441
Tomislav Simovic <116288908+shz0@users.noreply.github.com>
2024-09-09 11:30:36 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -3,13 +3,15 @@ from lagerbestand import Lagerbestand
 from lagerverwaltung import Lagerverwaltung
 from get_valid import get_valid_input
 
+#testing if function works separtly
+'''
 def get_valid_input(prompt: str, validate_func, error_msg: str) -> str:
     while True:
         user_input = input(prompt)
         if validate_func(user_input):
             return user_input
         print(error_msg)
-
+'''
 
 if __name__ == "__main__":
     verwaltung = Lagerverwaltung()
