3eeb65e426d5596bcc930d90961b6296068ce065
Tomislav Simovic <116288908+tsimovic1987@users.noreply.github.com>
2024-09-25 15:16:20 +0200

    Create division.py

---
+++ division.py
@@ -0,0 +1,21 @@
+class RawPart:
+    def __init__(self, plastic, propellant, status_high=False, status_ground=False):
+        self.plastic = plastic
+        self.propellant = propellant
+        self.status_high = status_high
+        self.status_ground = status_ground
+        
+    def set_location():
+        pass
+
+class PaintedPart:
+    pass
+
+class PurchasingPart:
+    pass
+
+class ProductionPart:
+    pass
+
+class StorageLocation:
+    pass
